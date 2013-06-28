af71c786b887f0e9ce9541b5d4934515094f659b
Vijay Mahrra <vijay.mahrra@gmail.com>
2013-06-29 00:43:08 +0200

    add example string helper

---
+++ htdocs/app/classes/helpers/string.php
@@ -0,0 +1,28 @@
+<?php
+namespace helpers;
+
+/**
+ * Base Helper Class
+ *
+ * @package helpers
+ * @author Vijay Mahrra <vijay.mahrra@gmail.com>
+ * @copyright (c) Copyright 2013 Vijay Mahrra
+ * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ */
+
+class String extends \Prefab {
+
+    /**
+     * random string producer
+     *
+     * @example $string = \helpers\String::instance()->randomString();
+     * @author Sascha Ohms
+     * @author Philipp Hirsch
+     * @copyright Copyright 2011, Bugtrckr-Team
+     * @license http://www.gnu.org/licenses/lgpl.txt
+     */
+    public static function randomString($length = 8)
+    {
+        return substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, $length);
+    }
+}
