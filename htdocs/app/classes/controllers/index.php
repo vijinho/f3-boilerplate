ba783e38d36f4ff1e6b2a2d3631e310fc465f2bf
Vijay Mahrra <vijay.mahrra@gmail.com>
2013-06-29 01:10:59 +0200

    rename php .htm templates to .phtml

--- htdocs/app/classes/controllers/index.php
+++ htdocs/app/classes/controllers/index.php
@@ -7,7 +7,7 @@ namespace controllers;
  * @package controllers
  * @author Vijay Mahrra <vijay.mahrra@gmail.com>
  * @copyright (c) Copyright 2013 Vijay Mahrra
- * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.phtmll)
  */
 
 class Index extends Base {
@@ -17,19 +17,19 @@ class Index extends Base {
     }
 
     public function home() {
-        echo \View::instance()->render('views/index/home.htm');
+        echo \View::instance()->render('views/index/home.phtml');
     }
 
     public function about() {
-        echo \View::instance()->render('views/index/about.htm');
+        echo \View::instance()->render('views/index/about.phtml');
     }
 
     public function contact() {
-        echo \View::instance()->render('views/index/contact.htm');
+        echo \View::instance()->render('views/index/contact.phtml');
     }
 
     public function credits() {
-        echo \View::instance()->render('views/index/credits.htm');
+        echo \View::instance()->render('views/index/credits.phtml');
     }
 
 }
