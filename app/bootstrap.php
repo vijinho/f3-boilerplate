e6f1c5ed35dbc71f7f6feab04a518f5581df0866
Vijay Mahrra <vijay@yoyo.org>
2013-07-02 14:54:59 +0200

    clarify calculation by placing in brackets

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -61,5 +61,5 @@ if ($f3->get('keep_notifications') === false) {
 
 // log script execution time if debugging
 if ($f3->get('DEBUG') || $f3->get('application.environment') == 'development') {
-    \Registry::get('logger')->write('Script executed in ' . microtime(true) - $f3->get('TIME') . ' seconds');
+    \Registry::get('logger')->write('Script executed in ' . (microtime(true) - $f3->get('TIME')) . ' seconds');
 }
