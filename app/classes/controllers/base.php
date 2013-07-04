7a646f0eb0302fee9b7050f34f19b5401d0de4e1
Vijay Mahrra <vijay@yoyo.org>
2013-07-04 16:44:14 +0200

    new method to send a json response and to format unix timestamp into a http header format date time

--- app/classes/controllers/base.php
+++ app/classes/controllers/base.php
@@ -36,4 +36,42 @@ abstract class Base {
         $this->logger = \Registry::get('logger');
     }
 
+    /*
+     * Utility to convert timestamp into a http header date
+     *
+     * @param int time php time value
+     * @param string $zone timezone, default GMT
+     */
+    protected function http_header_date($time = null, $zone = 'GMT') {
+        // use current time if bad time value
+        $time = (int) $time;
+        if (empty($time) || $time < 0) {
+            $time = null;
+        }
+        // if its not a 3 letter timezone set it to GMT
+        $zone = strtoupper($zone);
+        if (strlen($zone) != 3) {
+            $zone = 'GMT';
+        }
+        return gmdate("D, M d Y H:i:s", $time) . ' ' . $zone;
+    }
+
+    /*
+     * return the input paramater as a json output request
+     *
+     * @param mixed $x input variable, takes origin, age, methods
+     * @see http://www.w3.org/TR/2008/WD-access-control-20080912/
+     */
+    protected function send_json($x, $params = array()) {
+        $ttl = (array_key_exists('ttl', $params) ? $params['ttl'] : $this->f3->get('ajax.ttl')); // cache for $ttl seconds
+        if (empty($ttl))
+            header('Cache-Control: no-cache, must-revalidate');
+        $methods = (array_key_exists('methods', $params) ? $params['methods'] : 'GET, POST'); // Don't allow PUT, DELETE by default
+        header('Expires:  ' . self::http_header_date(array_key_exists('expires', $params) ? $params['expires'] : time() + $ttl));
+        header('Content-type: application/json');
+        header('Access-Control-Allow-Origin: ' . (array_key_exists('origin', $params) ? $params['origin'] : $this->f3->get('REALM')));
+        header('Access-Control-Max-Age: ' . $ttl);
+        header('Access-Control-Allow-Methods: ' .  $methods);
+        echo json_encode($x, ($this->f3->get('DEBUG') ? JSON_PRETTY_PRINT : null));
+    }
 }
