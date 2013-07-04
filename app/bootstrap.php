4dd8065183cafbc1a5323b7cb7262184bc0c7355
Vijay Mahrra <vijay.mahrra@gmail.com>
2013-07-05 00:24:12 +0200

    fix AUTOLOAD include path

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -20,7 +20,7 @@ if (file_exists('config/config.ini'))
 
 // setup class autoloader
 // @see http://fatfreeframework.com/quick-reference#autoload
-$f3->set('AUTOLOAD', __dir__.';../include/fatfree/lib/;classes/;include/');
+$f3->set('AUTOLOAD', __dir__.';../include/fatfree/lib/;classes/;../include/');
 
 // setup application logging
 \Registry::set('logger', new \Log($f3->get('application.logfile')));
