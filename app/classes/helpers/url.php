5b7d6928666a991b8d23f33d28e35b9a68a7c663
Vijay Mahrra <vijay.mahrra@gmail.com>
2013-07-21 15:59:12 +0200

    add method to get the url in an i18n format

--- app/classes/helpers/url.php
+++ app/classes/helpers/url.php
@@ -15,7 +15,6 @@ class URL extends \Prefab {
 	/**
 	 * Get the base url of the website
 	 *
-     *
 	 * @return string $path the base website url
 	 * @author Vijay Mahrra
      */
@@ -26,5 +25,22 @@ class URL extends \Prefab {
             return $matches[1];
         }
         return false;
+    }
+
+    /**
+     * Get an i18n URL of the website in the format: http://example.org/en/gb/some_url
+     *
+     * @param string $url
+     * @param string optional $language the language-country takes priority, eg. with 'sv-se' 'se' becomes the country
+     * @param string optional $country
+     * @return string $path the base website url
+     * @author Vijay Mahrra
+     */
+    public static function i18n($url = '', $language = null, $country = null)
+    {
+        // get the user language if not set
+        $language = substr((empty($language)) ? \F3::instance()->get('LANGUAGE') : $language, 0, 2);
+        $country = (empty($country)) ? \F3::instance()->get('COUNTRY') : $country;
+        return self::base() . '/' . $language . '/' . $country . '/' . $url;
     }
 }
