45b0e239b02a1e206580dbc8bf44d1df22801476
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-03-30 19:22:44 +0100

    BUGFIX: enabled homepage to show even if config.ini not there instead of error 500

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -38,38 +38,44 @@ if (PHP_SAPI !== 'cli' && empty($debug)) {
 }
 
 // setup application logging
-$logger = new \Log($f3->get('application.logfile'));
-\Registry::set('logger', $logger);
+$logfile = $f3->get('application.logfile');
+if (!empty($logfile)) {
+    $logger = new \Log($logfile);
+    \Registry::set('logger', $logger);
+}
 
 // setup database connection params
 // @see http://fatfreeframework.com/databases
-if ($f3->get('db.driver') == 'sqlite') {
-    $dsn = $f3->get('db.dsn');
-    $dsn = substr($dsn, 0, strpos($dsn, '/')) . realpath('../') . substr($dsn, strpos($dsn, '/'));
-    $db = new \DB\SQL($dsn);
-    // attach any other sqlite databases - this example uses the full pathname to the db
-    if ($f3->exists('db.sqlite.attached')) {
-        $attached = $f3->get('db.sqlite.attached');
-        $st = $db->prepare('ATTACH :filename AS :dbname');
-        foreach ($attached as $dbname => $filename) 
-            $st->execute(array(':filename' => $filename, ':dbname' => $dbname));
-    }    
-} else {
-    if (!$f3->get('db.dsn'))
-        $f3->set('db.dsn', sprintf("%s:host=%s;port=%d;dbname=%s",
-            $f3->get('db.driver'), 
-            $f3->get('db.hostname'), 
-            $f3->get('db.port'), 
-            $f3->get('db.name'))
+$db_enabled = !empty($f3->get('db.driver') || $f3->get('db.dsn'));
+if ($db_enabled) {
+    if ($f3->get('db.driver') == 'sqlite') {
+        $dsn = $f3->get('db.dsn');
+        $dsn = substr($dsn, 0, strpos($dsn, '/')) . realpath('../') . substr($dsn, strpos($dsn, '/'));
+        $db = new \DB\SQL($dsn);
+        // attach any other sqlite databases - this example uses the full pathname to the db
+        if ($f3->exists('db.sqlite.attached')) {
+            $attached = $f3->get('db.sqlite.attached');
+            $st = $db->prepare('ATTACH :filename AS :dbname');
+            foreach ($attached as $dbname => $filename) 
+                $st->execute(array(':filename' => $filename, ':dbname' => $dbname));
+        }    
+    } else {
+        if (!$f3->get('db.dsn'))
+            $f3->set('db.dsn', sprintf("%s:host=%s;port=%d;dbname=%s",
+                $f3->get('db.driver'), 
+                $f3->get('db.hostname'), 
+                $f3->get('db.port'), 
+                $f3->get('db.name'))
+            );
+
+        $db = new \DB\SQL(
+            $f3->get('db.dsn'), 
+            $f3->get('db.username'), 
+            $f3->get('db.password')
         );
-    
-    $db = new \DB\SQL(
-        $f3->get('db.dsn'), 
-        $f3->get('db.username'), 
-        $f3->get('db.password')
-    );
+    }
+    \Registry::set('db', $db);
 }
-\Registry::set('db', $db);
 
 // setup outgoing email server for php mail command
 ini_set("SMTP", $f3->get('email.host'));
