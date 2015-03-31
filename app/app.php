a2d7852a1fee8ab6e95144b40f6bafffde8a46d3
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-03-31 18:18:49 +0100

    remove unused line

--- app/app.php
+++ app/app.php
@@ -17,7 +17,6 @@ function Run()
     $f3 = require_once('../vendor/bcosca/fatfree/lib/base.php');
     $f3->set('AUTOLOAD', __dir__.';../vendor/bcosca/fatfree/lib/;lib/;../vendor/');
     Main::start($f3);
-    $logger = &$f3->ref('logger');
 
         // cli start
     if (PHP_SAPI == 'cli') {
