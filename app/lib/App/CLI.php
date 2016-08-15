bb12e92dd0bceeb5d3f9a4060817028d1273a16a
Vijay Mahrra <vijay.mahrra@gmail.com>
2016-08-15 13:34:19 +0100

    update cli.php

--- app/lib/App/CLI.php
+++ app/lib/App/CLI.php
@@ -6,8 +6,48 @@ if (PHP_SAPI !== 'cli') {
     die("This can only be executed in CLI mode.");
 }
 
-// run from app main folder
-chdir(realpath(__DIR__ . '/../../'));
-require_once '../lib/autoload.php';
-require_once 'lib/App/app.php';
-Run();
+/**
+ * Load, configure, run application
+ */
+function boot()
+{
+    if (session_status() == PHP_SESSION_NONE) {
+        ini_set('session.auto_start', 'Off');
+    } else {
+        ini_set('session.lazy_write', 'On');
+    }
+
+    chdir(realpath(__DIR__ . '/../../'));
+    require_once '../lib/autoload.php';
+
+    // bootstrap initial environment
+    $f3 = \Base::instance();
+    \FFMVC\App::start();
+    $f3->set('UNLOAD', function () {
+        \FFMVC\App::finish();
+    });
+
+    // load dependency injection container
+    $dice = new \Dice\Dice;
+
+    // database connection used by app
+    $dbConfig = $f3->get('db');
+    $dice->addRule('DB\\SQL', ['shared' => true, 'constructParams' => [
+        \FFMVC\Helpers\DB::createDbDsn($dbConfig),
+        $dbConfig['user'],
+        $dbConfig['pass'],
+        [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+    ]]);
+
+    // logging for application
+    $logfile = $f3->get('app.logfile');
+    $dice->addRule('Log', ['shared' => true, 'constructParams' => [$logfile]]);
+
+    // run the main application
+    require_once 'lib/App/App.php';
+    $app = $dice->create('\App\App');
+    $app->Main();
+}
+
+// run the application
+boot();
