a795da1a37fb0be8c9b7a7018d120d30fbb90b4b
Vijay Mahrra <vijay.mahrra@gmail.com>
2016-08-26 21:42:50 +0100

    refactored, simplified, added comments on what to do

--- app/lib/App/Controllers/API/API.php
+++ app/lib/App/Controllers/API/API.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers\API;
 
-use App\Models as Models;
-use FFMVC\Helpers as Helpers;
+use FFMVC\Helpers;
+
 
 /**
- * API Controller Class.
+ * Api Controller Class.
  *
  * @author Vijay Mahrra <vijay@yoyo.org>
  * @copyright Vijay Mahrra
@@ -14,13 +14,6 @@ use FFMVC\Helpers as Helpers;
  */
 class API
 {
-    /**
-     * version.
-     *
-     * @var version
-     */
-    protected $version;
-
     /**
      * response errors
      * 1xx: Informational - Transfer Protocol Information
@@ -38,44 +31,23 @@ class API
      *     - 406 - not acceptable (e.g. not in correct format like json)
      * 5xx: Server Error - The server was responsible.
      *
-     * @var errors
+     * @var array errors
      */
     protected $errors = [];
 
     /**
      * response data.
      *
-     * @var data
+     * @var array data
      */
     protected $data = [];
 
     /**
      * response params.
      *
-     * @var params
+     * @var array params
      */
     protected $params = [];
-
-    /**
-     * response helper object.
-     *
-     * @var response
-     */
-    protected $responseObject;
-
-    /**
-     * logger object.
-     *
-     * @var response
-     */
-    protected $loggerObject;
-
-    /**
-     * db.
-     *
-     * @var db
-     */
-    protected $db;
 
     /**
      * Error format required by RFC6794.
@@ -89,123 +61,123 @@ class API
             'description' => 'The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed.',
             'uri' => '',
             'state' => '',
+            'status' => 400
         ],
         'invalid_credentials' => [
             'code' => 'invalid_credentials',
             'description' => 'Credentials for authentication were invalid.',
             'uri' => '',
             'state' => '',
+            'status' => 403
         ],
         'invalid_client' => [
             'code' => 'invalid_client',
             'description' => 'Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method).',
             'uri' => '',
             'state' => '',
+            'status' => 401
         ],
         'invalid_grant' => [
             'code' => 'invalid_grant',
             'description' => 'The provided authorization grant (e.g., authorization code, resource owner credentials) or refresh token is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.',
             'uri' => '',
             'state' => '',
+            'status' => 401
         ],
         'unsupported_grant_type' => [
             'code' => 'unsupported_grant_type',
             'description' => 'The authorization grant type is not supported by the authorization server.',
             'uri' => '',
             'state' => '',
+            'status' => 400
         ],
         'unauthorized_client' => [
             'code' => 'unauthorized_client',
             'description' => 'The client is not authorized to request an authorization code using this method.',
             'uri' => '',
             'state' => '',
+            'status' => 401
         ],
         'access_denied' => [
             'code' => 'access_denied',
             'description' => 'The resource owner or authorization server denied the request.',
             'uri' => '',
             'state' => '',
+            'status' => 400
         ],
         'unsupported_response_type' => [
             'code' => 'unsupported_response_type',
             'description' => 'The authorization server does not support obtaining an authorization code using this method.',
             'uri' => '',
             'state' => '',
+            'status' => 400
         ],
         'invalid_scope' => [
             'code' => 'invalid_scope',
             'description' => 'The requested scope is invalid, unknown, or malformed.',
             'uri' => '',
             'state' => '',
+            'status' => 400
         ],
         'server_error' => [
             'code' => 'server_error',
             'description' => 'The authorization server encountered an unexpected condition that prevented it from fulfilling the request.',
             'uri' => '',
             'state' => '',
+            'status' => 500
         ],
         'temporarily_unavailable' => [
             'code' => 'temporarily_unavailable',
             'description' => 'The authorization server is currently unable to handle the request due to a temporary overloading or maintenance of the server.',
             'uri' => '',
             'state' => '',
+            'status' => 400
         ],
     ];
 
     /**
      * The OAuth Error to return if an OAuthError occurs.
      *
-     * @var bool|array OAuthError
+     * @var boolean|array OAuthError
      */
     protected $OAuthError = null;
 
-     /**
-      * init
-      * @param \Base $f3
-      */
-    public function __construct($f3)
+    /**
+     * initialize
+     */
+    public function __construct()
     {
-        $this->db = \Registry::get('db');
-        $this->version = $f3->get('api.version');
-        $this->responseObject = Helpers\Response::instance();
-        $this->loggerObject = \Registry::get('logger');
+        $this->params['http_status'] = 200;
     }
 
-     /**
-      * compile and send the json response.
-      *
-      * @param \Base $f3
-      * @return void
-      */
+    /**
+     * compile and send the json response.
+     *
+     * @param \Base $f3
+     * @return void
+     */
     public function afterRoute(\Base $f3)
     {
         $this->params['headers'] = empty($this->params['headers']) ? [] : $this->params['headers'];
-        $this->params['headers'] = [
-            'Version' => $f3->get('app.api_version'),
-        ];
-
-        $data = [];
+        $this->params['headers']['Version'] = $f3->get('api.version');
 
         // if an OAuthError is set, return that too
+        $data = [];
         if (!empty($this->OAuthError)) {
             $data['error'] = $this->OAuthError;
         }
 
         if (count($this->errors)) {
-            ksort($this->errors);
             foreach ($this->errors as $code => $message) {
                 $data['error']['errors'][] = [
                     'code' => $code,
                     'message' => $message
                 ];
             }
+            ksort($this->errors);
         }
 
-        if (is_array($this->data)) {
-            $data = array_merge($data, $this->data);
-        }
-
-        $this->responseObject->json($data, $this->params);
+        Helpers\Response::json(array_merge($data, $this->data), $this->params);
     }
 
     /**
@@ -213,9 +185,10 @@ class API
      *
      * @param string $code        the error code
      * @param string $message     the error message
-     * @param integer|null    $http_status the http status code
+     * @param null|int    $http_status the http status code
+     * @return void
      */
-    public function failure($code, $message, $http_status = null)
+    public function failure(string $code, string $message, int $http_status = null)
     {
         $this->errors[$code] = $message;
 
@@ -229,7 +202,7 @@ class API
      *
      * @param string $type
      *
-     * @return mixed array error type or boolean false
+     * @return array|bool error type or boolean false
      */
     protected function getOAuthErrorType(string $type)
     {
@@ -240,44 +213,18 @@ class API
      * Set the RFC-compliant OAuth Error to return.
      *
      * @param string $code  of error code from RFC
-     * @throws Models\APIServerException
-     * @return the OAuth error array
+     * @return array|boolean the OAuth error array
      */
     public function setOAuthError(string $code)
     {
-        $error = $this->getOAuthErrorType($code);
-        if (empty($error)) {
-            throw new APIServerException('Invalid OAuth error type.', 5100);
-        } else {
-            $this->OAuthError = $error;
-            // only set https status if not set anywhere else
-            if (!empty($this->params['http_status']) && $this->params['http_status'] !== 200) {
-                return $error;
-            }
+        $this->OAuthError = $this->getOAuthErrorType($code);
 
-            switch ($code) {
-
-                case 'invalid_client': // as per-spec
-                case 'invalid_grant':
-                case 'unauthorized_client':
-                    $this->params['http_status'] = 401;
-                    break;
-
-                case 'server_error':
-                    $this->params['http_status'] = 500;
-                    break;
-
-                case 'invalid_credentials':
-                    $this->params['http_status'] = 403;
-                    break;
-
-                default:
-                    $this->params['http_status'] = 400;
-                    break;
-            }
+        // only set https status if not set anywhere else
+        if ($this->params['http_status'] == 200) {
+            $this->params['http_status'] = $this->OAuthError['status'];
         }
 
-        return $error;
+        return $this->OAuthError;
     }
 
     /**
@@ -286,16 +233,16 @@ class API
      * Check that the credentials match the database
      * Cache result for 30 seconds.
      *
-     * @return boolean success/failure
+     * @return bool success/failure
      */
-    public function basicAuthenticateLoginPassword(): boolean
+    public function basicAuthenticateLoginPassword(): bool
     {
         $auth = new \Auth(new \DB\SQL\Mapper(\Registry::get('db'), 'users', ['email', 'password'], 30), [
             'id' => 'email',
             'pw' => 'password',
         ]);
 
-        return (int) $auth->basic(function ($pw) {
+        return (bool) $auth->basic(function ($pw) {
             return Helpers\Str::password($pw);
         });
     }
@@ -306,14 +253,15 @@ class API
      * Check that the credentials match a registered app
      * @param string $clientId the client id to check
      * @param string $clientSecret the client secret to check
-     * @return boolean success/failure
+     * @return bool success/failure
      */
-    public function authenticateClientIdSecret($clientId, $clientSecret): boolean
+    public function authenticateClientIdSecret(string $clientId, string $clientSecret): bool
     {
         if (empty($clientId) || empty($clientSecret)) {
             return false;
         }
-        // check fields, return boolean
+        // validate client_id/secret
+        return false;
     }
 
     /**
@@ -321,9 +269,9 @@ class API
      *
      * Check that the credentials match a registered app
      *
-     * @return boolean success/failure
+     * @return bool success/failure
      */
-    public function basicAuthenticateClientIdSecret(): boolean
+    public function basicAuthenticateClientIdSecret(): bool
     {
         $f3 = \Base::instance();
         return $this->authenticateClientIdSecret($f3->get('REQUEST.PHP_AUTH_USER'), $f3->get('REQUEST.PHP_AUTH_PW'));
@@ -331,6 +279,16 @@ class API
 
     /**
      * Validate the provided access token or get the bearer token from the incoming http request
+     * do $f3->set('access_token') if OK.
+     *
+     * Or login using app token with HTTP Auth using one of
+     *
+     * email:password
+     * email:access_token
+     *
+     * Or by URL query string param - ?access_token=$access_token
+     *
+     * Sets hive vars: user[] (mandatory), api_app[] (optional) and userScopes[]
      *
      * @return null|boolean true/false on valid access credentials
      */
@@ -338,42 +296,49 @@ class API
     {
         $f3 = \Base::instance();
 
-        // if forcing access to https die
+        // return if forcing access to https and not https
         if ('http' == $f3->get('SCHEME') && !empty($f3->get('api.https'))) {
             $this->failure('api_connection_error', "Connection only allowed via HTTPS!", 400);
             $this->setOAuthError('unauthorized_client');
-            return;
+
+            return false;
         }
 
-        $userAuthenticated = false;
+        // get token from request to set the user and app
+        // override if anything in basic auth or client_id/secret AFTER
         $token = $f3->get('REQUEST.access_token');
         if (!empty($token)) {
-            // fetch token and check
-            //$userAuthenticated = true;
+            // token does not exist!
+            $tokenExists = null; // perform a real check here against your db!
+            if (null == $tokenExists) {
+                $this->failure('authentication_error', "The token does not exist!", 401);
+                $this->setOAuthError('invalid_grant');
+
+                return false;
+            }
+            // check token is not out-of-date
+            $expires = 0; // get the expiry from your db!
+            if (time() > $expires) {
+                $this->failure('authentication_error', "The token expired!", 401);
+                $this->setOAuthError('invalid_grant');
+
+                return false;
+            }
+            // if token found load the user for the token from your db!
         }
 
-        // login with client_id and client_secret in request
-        $clientId = $f3->get('REQUEST.client_id');
-        $clientSecret = $f3->get('REQUEST.client_secret');
-        if (!empty($clientId) && !empty($clientSecret)
-                && $this->authenticateClientIdSecret($clientId, $clientSecret)) {
-            $userAuthenticated = true;
-        }
-
-        // check if login via http basic auth
-        $phpAuthUser = $f3->get('REQUEST.PHP_AUTH_USER');
-        if (!empty($phpAuthUser)) {
-            // try to login as email:password
-            if ($this->basicAuthenticateLoginPassword()) {
-                $userAuthenticated = true;
-            } elseif ($this->basicAuthenticateClientIdSecret()) {
-                $userAuthenticated = true;
+        // login with client_id and client_secret in request OR login/password using basic auth
+        if ($f3->get('REQUEST.PHP_AUTH_USER')) {
+            if ($this->basicAuthenticateClientIdSecret()) {
+            } elseif ($this->basicAuthenticateLoginPassword()) {
             }
         }
 
+        $userAuthenticated = false; // do a proper check here based on the above
         if (!$userAuthenticated) {
             $this->failure('authentication_error', "Not possible to authenticate the request.", 400);
             $this->setOAuthError('invalid_credentials');
+
             return false;
         }
 
@@ -381,7 +346,8 @@ class API
     }
 
     /**
-     * unknown catch-all api method
+     * catch-all
+     *
      * @return void
      */
     public function unknown()
@@ -389,8 +355,5 @@ class API
         $this->setOAuthError('invalid_request');
         $this->failure('api_connection_error', 'Unknown API Request', 400);
     }
-}
 
-class APIServerException extends \Exception
-{
-};
+}
